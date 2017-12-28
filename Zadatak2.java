805b155d0e2e154c69aa4ab267523da0ab0ca9b4
Branislav Manojlovic <manojlobaki@gmail.com>
2017-12-28 15:33:25 +0100

    Add files via upload

--- Zadatak2.java
+++ Zadatak2.java
@@ -6,11 +6,11 @@
 package zadatak2;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
 /**
@@ -19,11 +19,25 @@ import javax.swing.border.EmptyBorder;
  */
 public class Zadatak2 extends JFrame{
     
+    JPanel panel = new JPanel();
+    JPanel panel2 = new JPanel();
+    JPanel panel3 = new JPanel();
+    JLabel labela1 = new JLabel("Enter rainfall measurement separated by spaces");
+    JTextArea textPovrsina = new JTextArea("1 0 0 0 4 7 1 \n 0 0 1 0 1 1 2 8");
+    JButton dugme = new JButton("Calculate statistic");
+    JLabel labela2 = new JLabel("Total");
+    JLabel labela3 = new JLabel("Number");
+    JLabel labela4 = new JLabel("Average");
+    JTextField tekstPolje1 = new JTextField();
+    JTextField tekstPolje2 = new JTextField();
+    JTextField tekstPolje3 = new JTextField();  
+    
     public static void main(String[] args) {
-       new Zadatak2();
+        new Zadatak2();
     }
 
     public Zadatak2() {
+        
         setTitle("Rain Statistic");
         setSize(360, 230);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -31,35 +45,33 @@ public class Zadatak2 extends JFrame{
         setupGUI();
         setResizable(false);
         setVisible(true);
-    }
- 
-    public void setupGUI() {
-
-        JPanel centerPanel = new JPanel(new FlowLayout());
-        centerPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 1, 1));
-        centerPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-        centerPanel.add(new JLabel("Enter rainfall measurement separated by spaces"));
-        add(centerPanel, BorderLayout.NORTH);
-
-        JPanel centerPanel1 = new JPanel(new GridLayout(3, 1, 5, 5));
-        //centerPanel1.setBorder(new EmptyBorder(5, 5, 5, 5));
-        centerPanel1.add(new JTextArea("1 0 0 0 4 7 1 \n 0 0 1 0 1 1 2 8"));
-    //    add(centerPanel1, BorderLayout.WEST);
         
-    //    JPanel centerPanel3 = new JPanel(new GridLayout(3, 1, 5, 5));
-        centerPanel1.setBorder(new EmptyBorder(5, 5, 5, 50));
-        centerPanel1.add(new Button("Calculate statistic"));
-        add(centerPanel1, BorderLayout.CENTER);
-
-        JPanel centerPanel2 = new JPanel(new GridLayout(3, 2, 5, 5));
-        centerPanel2.add(new JLabel("Total"));
-        centerPanel2.add(new JTextField(1));
-        centerPanel2.add(new JLabel("Number"));
-        centerPanel2.add(new JTextField(1));
-        centerPanel2.add(new JLabel("Average"));
-        centerPanel2.add(new JTextField(1));
-        centerPanel2.setBorder(new EmptyBorder(5, 5, 5, 200));
-        add(centerPanel2, BorderLayout.SOUTH);
-
+    }
+    
+    public void setupGUI() {
+        
+        panel.setBorder(new EmptyBorder(new Insets(5, 5, 5, 5)));
+        panel.add(labela1);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 1, 1));
+        
+        panel2.setLayout(new GridLayout(2, 1, 5, 5));
+        panel2.setBorder(new EmptyBorder(5, 5, 5, 100));
+        panel2.add(textPovrsina);
+        panel2.add(dugme);
+      
+        
+        panel3.setLayout(new GridLayout(3, 2, 5, 5));
+        panel3.setBorder(new EmptyBorder(5, 5, 5, 220));
+        panel3.add(labela2);
+        panel3.add(tekstPolje1);
+        panel3.add(labela3);
+        panel3.add(tekstPolje2);
+        panel3.add(labela4);
+        panel3.add(tekstPolje3);
+        
+        
+        add(panel, BorderLayout.NORTH);
+        add(panel2, BorderLayout.CENTER);
+        add(panel3, BorderLayout.SOUTH);
     }
 }
--- Zadatak3.java
+++ Zadatak3.java
@@ -3,12 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package zadatak2;
+package zadatak3;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
@@ -21,56 +25,76 @@ public class Zadatak3 extends JFrame{
     JPanel panel = new JPanel();
     JPanel panel2 = new JPanel();
     JPanel panel3 = new JPanel();
-    JLabel labela1 = new JLabel("Enter rainfall measurement separated by spaces");
-    JTextArea textPovrsina = new JTextArea("1 0 0 0 4 7 1 \n 0 0 1 0 1 1 2 8");
-    JButton dugme = new JButton("Calculate statistic");
-    JLabel labela2 = new JLabel("Total");
-    JLabel labela3 = new JLabel("Number");
-    JLabel labela4 = new JLabel("Average");
+    JPanel panel4 = new JPanel();
+    
+    JLabel labela2 = new JLabel("Amount of Loan");
     JTextField tekstPolje1 = new JTextField();
+    JLabel labela3 = new JLabel("Number of Years");
+    JTextField tekstPolje11 = new JTextField();
+    JLabel labela4 = new JLabel("Interest Rate");
+    // element za biranje!!!
+    JComboBox<String> kombo = new JComboBox<>();
+
+    JButton dugme1 = new JButton("Calculate");
+    JButton dugme2 = new JButton("Clear");
+    
+    JLabel labela5 = new JLabel("Monthly Payment");
     JTextField tekstPolje2 = new JTextField();
+    
+    JLabel labela6 = new JLabel("Monthly Payment");
     JTextField tekstPolje3 = new JTextField();  
+    
+    JButton dugme3 = new JButton("Quit");
     
     public static void main(String[] args) {
         new Zadatak3();
     }
 
     public Zadatak3() {
-        
-        setTitle("Rain Statistic");
-        setSize(360, 230);
+        setTitle("Loan Payment Calculator");
+        setSize(400, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setupGUI();
         setResizable(false);
         setVisible(true);
-        
     }
     
+    
+    
+    
+ 
     public void setupGUI() {
         
+               
+        panel2.setLayout(new GridLayout(8, 1, 10, 10));
+        panel3.setLayout(new GridLayout(5, 1, 10, 10));
+        panel4.setLayout(new GridLayout(2, 2, 10, 10));
+        
+       
         panel.setBorder(new EmptyBorder(new Insets(5, 5, 5, 5)));
-        panel.add(labela1);
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 1, 1));
+        // Za levi deo
+        panel2.add(labela2);
+        panel2.add(tekstPolje1);
+       // panel2.add(panel4);
+        panel4.add(labela3);
+        panel4.add(tekstPolje11);
+        panel4.add(labela4);
+        panel4.add(kombo);
+        panel2.add(dugme1);
+        panel2.add(dugme2);
         
-        panel2.setLayout(new GridLayout(2,1, 5, 5));
-        panel2.setBorder(new EmptyBorder(5, 5, 5, 100));
-        panel2.add(textPovrsina);
-        panel2.add(dugme);
-      
-        
-        panel3.setLayout(new GridLayout(3, 2, 5, 5));
-        panel3.setBorder(new EmptyBorder(5, 5, 5, 220));
-        panel3.add(labela2);
-        panel3.add(tekstPolje1);
-        panel3.add(labela3);
+        // Za desni deo
+        panel3.add(labela5);
         panel3.add(tekstPolje2);
-        panel3.add(labela4);
+        panel3.add(labela6);
         panel3.add(tekstPolje3);
+        panel3.add(dugme3);
         
+        panel.add(panel2, BorderLayout.WEST);
+        panel.add(panel4);
+        panel.add(panel3, BorderLayout.EAST);
+        add(panel);
         
-        add(panel, BorderLayout.NORTH);
-        add(panel2, BorderLayout.CENTER);
-        add(panel3, BorderLayout.SOUTH);
     }
 }
